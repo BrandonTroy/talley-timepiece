f6e85cec6fa6c483aeb055c90c495885737531f4
bjtroy <bjtroy@ncsu.edu>
2023-04-11 18:27:06 -0400

    refactored motor, client updated for production

--- source/client.py
+++ source/client.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 import pytz
 from threading import Thread
 from alarm import Alarm
-#from motor import Motor
 from requests import get
 from json import loads
 import drivers
@@ -12,7 +11,6 @@ import RPi.GPIO as GPIO
 
 
 BUTTON_PIN = 37
-MOTOR_PINS = (18, 22, 24, 26)
 
 # initialze the GPIO
 GPIO.setwarnings(False)
@@ -28,14 +26,12 @@ class App:
         Alarm.from_json(['00:00', None, False])
     ]
     display = drivers.Lcd()
-    #motor = Motor(MOTOR_PINS, 30)
 
     # initializes app and starts threads
     @staticmethod
     def start():
         Thread(target=App.fetch, daemon=True).start()
         Thread(target=App.input_listener, daemon=True).start()
-        #Thread(target=App.motor.perpetuate, daemon=True).start()
         App.tick()
     
     # updates the time every second based on the timezone
@@ -43,15 +39,15 @@ class App:
     def tick():
         while True:
             t = datetime.now(pytz.timezone(App.timezone))
-            
+            # check for alarm needing to go off
             for alarm in App.alarms:
                 if alarm.active and alarm.compare_time(t) and (time() - alarm.last_gone_off) >= 60:
                     Thread(target=alarm.go_off, daemon=True).start()
-            
-            time_string = datetime.strftime(t, "%I:%M:%S %p")
+            # update the display
+            time_string = datetime.strftime(t, "%I:%M %p")
             alarm_string = "  ".join(map(str, App.alarms))
-            App.display.lcd_display_string(time_string, 1)
-            App.display.lcd_display_string(alarm_string, 2)
+            App.display.lcd_display_string(time_string.center(16), 1)
+            App.display.lcd_display_string(alarm_string.center(16), 2)
             sleep(1)
     
     # fetches the app data from the server
@@ -63,11 +59,10 @@ class App:
             App.timezone = data['timezone']
             Alarm.counter = 0
             # update alarms only if they have changed
-            alarms = list(map(Alarm.from_json, data['alarms']))
-            for i, alarm in enumerate(App.alarms):
-                if alarm != alarms[i]:
-                    alarm.stop()
-                    App.alarms[i] = alarms[i]
+            for i, alarm in enumerate(map(Alarm.from_json, data['alarms'])):
+                if alarm != App.alarms[i]:
+                    App.alarms[i].stop()
+                    App.alarms[i] = alarm
             print("REQUEST:", result)
             print(data)
             sleep(1)
@@ -78,7 +73,7 @@ class App:
         while True:
             # wait for button down press (rising edge)
             while GPIO.input(37) == GPIO.LOW:
-                pass
+                sleep(0.1)
             # process button press
             if Alarm.current:
                 if Alarm.current.snoozed:
@@ -87,9 +82,12 @@ class App:
                     Alarm.current.snooze()
             # wait for button up press (falling edge)
             while GPIO.input(37) == GPIO.HIGH:
-                pass
+                sleep(0.1)
             
 
 
 if __name__ == '__main__':
-    App.start()
+    try:
+        App.start()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
--- source/motor.py
+++ source/motor.py
@@ -2,35 +2,34 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-class Motor:
-    def __init__(self, pins: tuple[int, int, int, int], rpm: int, steps: int = 200, mode: GPIO.Mode = GPIO.BOARD):
-        GPIO.setmode(mode)
-        for pin in pins:
-            GPIO.setup(pin, GPIO.OUT)
-        self.pins = pins
-        self.rpm = rpm
-        self.steps = steps
-        
-    def step(self, a, b, c, d):
-        GPIO.output(self.pins[0], a)
-        GPIO.output(self.pins[1], b)
-        GPIO.output(self.pins[2], c)
-        GPIO.output(self.pins[3], d)
-        
-    def rotate(self, steps: int = None):
-        if steps is None:
-            steps = self.steps
-        delay = 60 / self.steps / self.rpm
-        for i in range(steps // 4):
-            self.step(0, 1, 0, 1)
-            sleep(delay)
-            self.step(0, 1, 1, 0)
-            sleep(delay)
-            self.step(1, 0, 1, 0)
-            sleep(delay)
-            self.step(1, 0, 0, 1)
-            sleep(delay)
-            
-    def perpetuate(self):
-        while True:
-            self.rotate()
+pins = (18, 22, 24, 26)   # 4 GPIO pins which are motor inputs
+steps = 200   # number of steps per revolution of the motor
+rpm = 30   # desired revolutions per minute
+delay = 60 / steps / rpm   # delay between steps 
+
+# setup the GPIO pins
+GPIO.setmode(GPIO.BOARD)
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+
+# sets the state of the four input pins
+def step(a, b, c, d):
+    GPIO.output(pins[0], a)
+    GPIO.output(pins[1], b)
+    GPIO.output(pins[2], c)
+    GPIO.output(pins[3], d)
+
+try:
+    # infinitely repeat the four step pattern to rotate the motor
+    while True:
+        step(0, 1, 0, 1)
+        sleep(delay)
+        step(0, 1, 1, 0)
+        sleep(delay)
+        step(1, 0, 1, 0)
+        sleep(delay)
+        step(1, 0, 0, 1)
+        sleep(delay)
+except KeyboardInterrupt:
+    # clean up pins
+    step(0, 0, 0, 0)
